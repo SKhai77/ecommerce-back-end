009f0390d40ab9157433e49dbca661b510bf8d43
SKhai77 <orionkhai@gmail.com>
2023-08-31 14:27:07 -0500

    Added the fields and rule for the Cateory model

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,26 @@
+// Import necessary components from Sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
+// Define the Category class which extends Sequelize's Model class
 class Category extends Model {}
 
+// set up fields and rules for Category model
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes,INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes,STRING,
+      allowNull: false,
+    },    
   },
   {
     sequelize,
@@ -17,4 +31,5 @@ Category.init(
   }
 );
 
+// Export the Category class
 module.exports = Category;
