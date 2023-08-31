cdff4f8209d2d7bde53cb99ef259374d548c34f1
SKhai77 <orionkhai@gmail.com>
2023-08-31 17:09:05 -0500

    Added comments to the code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
+// Import the necessary modules
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Use the api routes for the '/api' endpoint
 router.use('/api', apiRoutes);
 
+// If the requested route doesn't match any defined routes, send a response
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
+// Export the router with the defined routes
 module.exports = router;
