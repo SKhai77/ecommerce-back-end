8e4f3d5ea7cc106ff9fa711a6bcba148f5741ce8
SKhai77 <orionkhai@gmail.com>
2023-08-31 14:30:23 -0500

    Added the fields and rules for Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,25 @@
+// Import necessary components from Sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
+// Define the Tag class which extends Sequelize's Model class
 class Tag extends Model {}
 
+// set up fields and rules for ProductTag model
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
@@ -17,4 +30,5 @@ Tag.init(
   }
 );
 
+// Export the Tag class
 module.exports = Tag;
