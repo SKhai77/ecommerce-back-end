af10dd0ec9a4d136de1a5ad14cafb224f4467c2e
SKhai77 <orionkhai@gmail.com>
2023-08-31 17:08:31 -0500

    Added comments to the code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
+// Import the necessary modules
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// Set up routes for the different endpoints
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+// Export router with the defined routes
 module.exports = router;
