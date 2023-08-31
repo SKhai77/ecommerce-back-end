71d57ef9777a57cd34cba7d8f67dba67c5ae1188
SKhai77 <orionkhai@gmail.com>
2023-08-31 14:29:13 -0500

    Added the fields and rules for ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,28 @@
+// Import necessary components from Sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
+// Define the Category class which extends Sequelize's Model class
 class ProductTag extends Model {}
 
+// set up fields and rules for ProductTag model
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+    },
   },
   {
     sequelize,
@@ -17,4 +33,5 @@ ProductTag.init(
   }
 );
 
+// Imporit the ProductTag class
 module.exports = ProductTag;
